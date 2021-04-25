5970023968b7a9d0ad078ad2a7e2103c225d9075
Jim Bruno Goldberg <goldberg@nekutima.eu>
2021-04-25 17:49:04 +0200

    reads rss from alexandria + inet

--- main.py
+++ main.py
@@ -1,33 +1,94 @@
+from gql import gql, Client
+from gql.transport.aiohttp import AIOHTTPTransport
 from pathlib import Path
 import yaml, os, feedparser
 
-root_path = ""
+# https://pypi.org/project/PyRSS2Gen/
 
-def warrant_path(path):
+def userspace_path(path):
     p = os.path.expanduser(path)
     if not os.path.isdir(p):
         os.mkdir(p)
     return p
 
 def process_RSS(source):
-    print("Processing RSS: " + source["name"])
-    rss_path = warrant_path( root_path + "/" + source["name"] )    
-    data = feedparser.parse(source["URL"])
-    print(data.feed.title)
+
+    if source['name'] is None:
+        print("Processing RSS: " + source['id'])
+    else:
+        print("Processing RSS: " + source['name'])
+
+    data = feedparser.parse(source["url"])
+    if source['name'] is None:
+        query = load_gql("update-rss")
+        params = {
+            "id": source['id'],
+            "name": data.feed.title,
+            "description": data.feed.description
+            }
+        res = alexandria.execute(query, variable_values=params)
+        source['name'] = res['updateRss']['rss']['name']
+        print(source['name'])
     
     for entry in data.entries:
-        print("    " + entry.title)
+        line = entry.published
+
+        if hasattr(entry, 'author'):
+            line += " ("+ entry.author +")"
+         
+        line +=":" + entry.title
+
+        print(line)
+
+def load_config():
+    global config  
+
+    home = str(Path.home())
+    stream =  open(home + "/.uhura.yml", "r")    
+    config = yaml.safe_load(stream)
+
+    config['root'] = userspace_path(config["root"])
+
+def load_gql(tag_name):
+    with open('graphql/'+tag_name + '.gql', 'r') as file:
+        data = file.read().replace('\n', '')
+    return gql(data)
+
+
+def connect_alexandria():    
+
+    global alexandria
+
+    # Create a GraphQL client using the defined transport
+    temp_client = Client(transport=AIOHTTPTransport(url=config['alexandria']['endpoint']), fetch_schema_from_transport=True)
+
+    query = load_gql("authenticate")
+    params = {
+        "email": config['alexandria']['user'],
+        "password": config['alexandria']['pwd']
+        }
+
+    token = temp_client.execute(query, variable_values=params)['authenticate']['jwt']
+
+    transport = AIOHTTPTransport(
+        url=config['alexandria']['endpoint'],
+        headers={'Authorization': 'Bearer ' + token})
+
+    alexandria = Client(transport=transport, fetch_schema_from_transport=True)
+
+def personal_rsses():
+    query = load_gql('rsses')
+    return alexandria.execute(query)['rsses']['nodes']
 
 
 if __name__ == "__main__":
+    print("Uhura 0.0.1")
 
-    home = str(Path.home())
-    stream =  open(home + "/.uhura.yml", "r")
-    config  = yaml.safe_load(stream)
+    load_config()
+    connect_alexandria()
 
-    root_path = warrant_path(config["root"])
-
-    for rss in config["sources"]:
+    for rss in personal_rsses():
         process_RSS(rss)
+
 
 
